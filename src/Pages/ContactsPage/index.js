518a52f45f7be23ef3132fb649f08f5530adf3ef
neel123go <ayounpaul1029@gmail.com>
2022-08-01 17:49:11 +0600

    Emplement search functionality in contacts page

---
+++ src/Pages/ContactsPage/index.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import contacts from '../../../assets/data/contacts.json';
+
+const ContactsPage = () => {
+    const [searchText, setSearchText] = useState('');
+    const [filteredContacts, setFilteredContacts] = useState(contacts);
+
+    useEffect(() => {
+        const newContact = contacts.filter(contact => contact.user_display_name.toLowerCase().includes(searchText.toLowerCase()));
+        setFilteredContacts(newContact);
+    }, [searchText]);
+
+    return (
+        <View style={styles.page}>
+            <TextInput value={searchText} onChangeText={setSearchText} style={styles.searchInput} placeholder='Search' />
+            <FlatList
+                data={filteredContacts}
+                renderItem={({ item }) => <Text style={styles.contactName}>{item.user_display_name}</Text>}
+                ItemSeparatorComponent={() => <View style={styles.separator} />}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    page: {
+        paddingVertical: 30,
+        paddingHorizontal: 20
+    },
+    contactName: {
+        fontSize: 18,
+        paddingVertical: 10,
+        color: '#404040'
+    },
+    separator: {
+        width: '100%',
+        height: 1,
+        backgroundColor: '#edebeb',
+    },
+    searchInput: {
+        backgroundColor: '#edebeb',
+        padding: 10,
+        borderRadius: 5,
+        fontSize: 18,
+        fontWeight: '500',
+        color: '#616060'
+    }
+});
+
+export default ContactsPage;
